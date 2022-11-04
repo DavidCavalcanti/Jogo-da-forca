b69d58aac46f71c5c6e97ccfba78ac662fb8307e
david.cavalcanti <david.cavalcanti@i.systems.com.br>
2022-11-04 15:39:58 -0300

    Adicionando cabeçalho

--- forca.cpp
+++ forca.cpp
@@ -20,6 +20,11 @@ bool letra_existe(char chute)
 
 int main()
 {
+
+    cout << "*********************" << endl;
+    cout << "*** Jogo da Forca ***" << endl;
+    cout << "*********************" << endl;
+
     bool nao_acertou = true;  // no começo não acertou a palavra secreta, então o valor é true
     bool nao_enforcou = true; // no começo ainda não perdeu, então recebe true;
 
