73dead861140e15a0f50bb01b1894ee31c4557fc
david.cavalcanti <david.cavalcanti@i.systems.com.br>
2022-11-04 10:56:55 -0300

    Adicionando função para verificar o chute

--- forca.cpp
+++ forca.cpp
@@ -2,11 +2,25 @@
 #include <string>
 using namespace std;
 
+const string PALAVRA_SECRETA = "MELANCIA";
+
+bool letra_existe(char chute)
+{
+    for (int i = 0; i < PALAVRA_SECRETA.size(); i++)
+    {
+        if (chute == PALAVRA_SECRETA[i])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main()
 {
-    string palavra_secreta = "MELANCIA";
+    
 
-    bool nao_acertou = true; // no começo não acertou a palavra secreta, então o valor é true
+    bool nao_acertou = true;  // no começo não acertou a palavra secreta, então o valor é true
     bool nao_enforcou = true; // no começo ainda não perdeu, então recebe true;
 
     while (nao_acertou && nao_enforcou)
@@ -14,7 +28,13 @@ int main()
         char chute;
         cin >> chute;
 
-        cout << "Seu chute foi " << chute << endl;
+        if (letra_existe(chute))
+        {
+            cout << "Voce acertou! Seu chute esta na palavra!" << endl;
+        }
+        else
+        {
+            cout << "Voce errou! Seu chute nao esta na palavra" << endl;
+        }
     }
-    
 }
